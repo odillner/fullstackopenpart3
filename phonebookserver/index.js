54a673e3929e40198ba78b1ff2e8f57aaae19050
Ossian Dillner <39240378+odillner@users.noreply.github.com>
2020-06-04 13:22:34 +0200

    phonebook server

--- phonebookserver/index.js
+++ phonebookserver/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 const app = express();
 const PORT = 3001;
 
 const routes = require('./routes');
 
 //middleware
+app.use(cors());
 app.use(express.json()); //body
 app.use(morgan('tiny')); //logger
 
